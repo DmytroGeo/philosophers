e3bf1b94ec799ea31a42c96e86da8821bafdf8a9
Dmytro Georgiyenko <dgeorgiy@student.42london.com>
2025-03-21 15:47:29 +0000

    experiment with gettimeofday

---
+++ philo/philo.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgeorgiy <dgeorgiy@student.42london.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/18 17:32:13 by dgeorgiy          #+#    #+#             */
+/*   Updated: 2025/03/18 17:43:41 by dgeorgiy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+#include <string.h> // memset,
+#include <stdio.h> // printf, 
+#include <stdlib.h> // malloc, free, 
+#include <unistd.h> // write, usleep
+#include <sys/time.h> // gettimeofday
+#include <pthread.h> 
+// pthread_create, pthread_detach, pthread_join, pthread_mutex_init, pthread_mutex_destroy, pthread_mutex_lock, pthread_mutex_unlock
+
+
+
+#endif
